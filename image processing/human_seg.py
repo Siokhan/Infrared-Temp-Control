2d78b10d6153a6be95e8758113b5d572ce50a112
Siokhan <siokhank@gmail.com>
2019-03-31 22:13:27 +0100

    histogram function, saving image functionality

--- image processing/human_seg.py
+++ image processing/human_seg.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from skimage import io
 from skimage import exposure
 from skimage.color import rgb2hed
+from skimage.color import hed2rgb
 from matplotlib.colors import LinearSegmentedColormap
 
 #Seperating human from the rest of the image using IHC colour deconvolution
 #This function accomplishes what is described above on specified image
-def color_seperation(image_path, colour1, colour2):
+def segment_histo(image_path, colour1, colour2):
     image = io.imread(image_path)
     image_hed = rgb2hed(image)
 
@@ -19,12 +20,27 @@ def color_seperation(image_path, colour1, colour2):
     human_seperation.set_title('colour deconvolution')
     human_seperation.axis('off')
 
-    #attempt at saving the image onto machine, not crucial
-    #having issues, come back to this at the end
-    #io.imsave('./images/' + image_path[9:17] + '_seg.jpg', final_seg)
+    #return histogram of segmented image split between RGB channels
 
-    print(final_seg.min(), final_seg.max())  
-    return human_seperation, final_seg
+    final_seg_rgb = hed2rgb(final_seg)
+
+    fig, rgb_histo = plt.subplots(nrows=3, ncols=1, figsize=(5, 8)) 
+
+    for c, c_color in enumerate(('red', 'green', 'blue')):
+        img_hist, bins = exposure.histogram(final_seg_rgb[..., c])
+        rgb_histo[c].plot(bins, img_hist / img_hist.max())
+        img_cdf, bins = exposure.cumulative_distribution(final_seg_rgb[..., c])
+        rgb_histo[c].plot(bins, img_cdf)
+        rgb_histo[c].set_ylabel(c_color) 
+
+    rgb_histo[0].set_title('Histogram of RGB channels')
+
+    #attempt at saving the image onto machine, not crucial
+    #massive precision loss look into conservation
+    io.imsave('./images/' + image_path[9:17] + '_seg.jpg', final_seg_rgb)
+    
+    print(final_seg_rgb.min(), final_seg_rgb.max())  
+    return human_seperation, final_seg, rgb_histo
 
 def infra_histogram(image):
    fig, rgb_histo = plt.subplots(nrows=3, ncols=1, figsize=(5, 8)) 
@@ -38,8 +54,7 @@ def infra_histogram(image):
 
     return rgb_histo
 
-image_seg = color_seperation('./images/atlantis.jpg', 'white', 'green')
-#image_histo = infra_histogram(final_seg)
+image_seg = segment_histo('./images/atlantis.jpg', 'white', 'green')
 
 plt.show()
 
